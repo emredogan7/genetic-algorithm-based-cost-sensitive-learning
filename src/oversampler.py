b1dc4b9d1776d3e50d6fd2afcee8267d535a30d6
Emre Doğan <emre.dogan@bilkent.edu.tr>
2020-01-12 09:56:17 +0300

    Update oversampler.py

--- src/oversampler.py
+++ src/oversampler.py
@@ -1,15 +1,17 @@
 import numpy as np
 import random
 
-def oversampler(data,factor):
+def oversampler(data, sampling_factor):
     ones_twos = np.zeros((0,22))
-    for z in data:
-        if (z[21] == 2 or z[21] == 1):
-            ones_twos = np.vstack([ones_twos,z])
-
-    newData = data
-    for _ in range(factor):
-        newData = np.vstack([newData,ones_twos])
+    for instance in data:
+        if (instance[21] == 2 or instance[21] == 1):
+            ones_twos = np.vstack([ones_twos,instance])
+    
+    oversampled_data
+    oversampled_data = data
+    
+    for _ in range(sampling_factor):
+        oversampled_data = np.vstack([oversampled_data,ones_twos])
         
-    random.shuffle(newData)
-    return newData
+    random.shuffle(oversampled_data)
+    return oversampled_data
